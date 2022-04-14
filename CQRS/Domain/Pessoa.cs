46b87a40ea4da4aa388465d6390ebfa939ac4a4e
Gabriel <gabrieltrevizoli@betalearning.com.br>
2022-04-14 15:13:26 -0300

    classe Pessoa correcao

---
+++ CQRS/Domain/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Domain
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public char Sexo { get; set; }
+    }
+}
