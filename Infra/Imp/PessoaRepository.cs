216ddbe1295656deea135dc0a971e01df4f7d511
Gabriel <gabrieltrevizoli@betalearning.com.br>
2022-04-14 15:24:31 -0300

    implementacaco IRepository<Pessoa>

---
+++ Infra/Imp/PessoaRepository.cs
@@ -0,0 +1,43 @@
+﻿using CQRS.Domain;
+using Infra.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Imp
+{
+    class PessoaRepository : IRepository<Pessoa>
+    {
+        private static Dictionary<int, Pessoa> pessoas = new Dictionary<int, Pessoa>();
+
+        public async Task<IEnumerable<Pessoa>> GetAll()
+        {
+            return await Task.Run(() => pessoas.Values.ToList());
+        }
+
+        public async Task<Pessoa> Get(int id)
+        {
+            return await Task.Run(() => pessoas.GetValueOrDefault(id));
+        }
+
+        public async Task Add(Pessoa pessoa)
+        {
+            await Task.Run(() => pessoas.Add(pessoa.Id, pessoa));
+        }
+
+        public async Task Edit(Pessoa pessoa)
+        {
+            await Task.Run(() =>
+            {
+                pessoas.Remove(pessoa.Id);
+                pessoas.Add(pessoa.Id, pessoa);
+            });
+        }
+
+        public async Task Delete(int id)
+        {
+            await Task.Run(() => pessoas.Remove(id));
+        }
+}
